4a323ee8a869a142cb3d088dfaa5380efd90c111
CedricKirst <Cedric.Kirst@t-online.de>
2016-12-05 10:28:21 +0100

    new exercisesheet 6

--- Methods/getJacobian.m
+++ Methods/getJacobian.m
@@ -5,8 +5,6 @@ deriv = linquadderivref(xi, eta);
 
 J = [nodes' * deriv(:,1) , nodes' * deriv(:,2)];
 detJ = det(J);
-%adj = [J(2,2) , -J(1,2);-J(2,1), J(1,1)];
-%invJ = adj/detJ;
 invJ = inv(J);
 
 end
